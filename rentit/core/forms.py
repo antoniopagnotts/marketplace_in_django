f733744fba173cce4a1d099aa928052ca5cad1d7
Antonio <antonio.pagnotta11@gmail.com>
2023-07-30 17:01:56 +0200

    added login page

--- rentit/core/forms.py
+++ rentit/core/forms.py
@@ -1,6 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-4 px-6 rounded-xl' #it makes the username field in the signup rounded and better to see
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'w-full py-4 px-6 rounded-xl' #it makes the username field in the signup rounded and better to see
+    }))
 
 class SignupForm(UserCreationForm):
     class Meta:
